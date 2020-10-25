3140674dd3881934e02cad3cab68e4a1564a8e1f
mezaisrael <meza.israel.91@gmail.com>
2020-10-25 16:40:41 -0400

    Add navigation buttons to overlay

---
+++ src/components/FurnitureList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function FurnitureList() {
+    return (
+        <div>
+            {//return all the furniture}
+        </div>
+    
+    )
+}
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import './Hero.css'
+import Button from './Button'
+
+import LeftArrow from '../images/iconfinder_arrow-left-01_186410.png'
+import RightArrow from '../images/iconfinder_arrow-right-01_186410.png'
 
 export default function Hero() {
+    // additional styling
+    const button = {
+        // margin: 'auto',
+        margin: '50px auto auto',
+        display: 'block'
+    }
+
     return (
         <div className='hero'>
             <div className='hero-overlay'>
+                <img id='home-page-nav-left' alt='left navigation arrow' src={LeftArrow} />
+                <img id='home-page-nav-right' alt='right navigation arrow' src={RightArrow} />
                 <h1 className='title'>Sofas</h1>
+                <div>
+                    <Button style={button} text='Buy Now'/>
+                </div>
             </div>
         </div>
     )
+
 }
