4a3c0bed138925dc2f3220c6913bb3cdad7fa544
mezaisrael <meza.israel.91@gmail.com>
2020-10-26 18:52:14 -0400

    apply css with javscript so that we can change backgroun

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import './Hero.css'
 import Button from './Button'
 
+import BedRoom from '../images/bedroom.jpg'
+import LivingRoom from '../images/livingRoom-mid.jpg'
+import Office from '../images/office.jpg'
+
 import LeftArrow from '../images/iconfinder_arrow-left-01_186410.png'
 import RightArrow from '../images/iconfinder_arrow-right-01_186410.png'
 
@@ -13,8 +17,21 @@ export default function Hero() {
         display: 'block'
     }
 
+    const hero = {
+        display: 'flex',
+        alignItems: 'center',
+        height: 'calc(100% - 169px)',
+        background: `url(${BedRoom}) center/cover no-repeat`,
+        backgroundSize: 'cover'
+    }
+
+
+    //TODO wait for view to render to target these
+    // let leftArrowEl = document.getElementById('home-page-nav-left')
+    // console.log(leftArrowEl)
+
     return (
-        <div className='hero'>
+        <div className='hero' style={hero}>
             <div className='hero-overlay'>
                 <img id='home-page-nav-left' alt='left navigation arrow' src={LeftArrow} />
                 <img id='home-page-nav-right' alt='right navigation arrow' src={RightArrow} />
